351d41a879837b7ccfa04ccd3e9d12db2cc7875e
Sergio Martín <alu0100611625@ull.edu.es>
2013-10-07 22:50:07 +0100

    Añadida funcion multiplicar

--- matrices.rb
+++ matrices.rb
@@ -1,6 +1,9 @@
+#!/bin/env ruby
+# encoding: utf-8
+
 system("clear")# Borramos la pantalla
-a=[[1,2,3],[4,5,6],[7,8,9]]
-b=[[4,5,6],[7,8,9],[0,1,2]]
+a=[[2,0,1],[3,0,0],[5,1,1]]
+b=[[1,0,1],[1,2,1],[1,1,0]]
 c=Array.new
 m,n = a.size, a[1].size
 	opcion=-1
@@ -16,6 +19,7 @@ m,n = a.size, a[1].size
 
 		if opcion == 1 
 			 
+<<<<<<< HEAD
 			for i in 0...m do 
 				c[i]= Array.new
 				for j in 0...n do
@@ -27,6 +31,18 @@ m,n = a.size, a[1].size
 				for j in 0...n do
 					c[i][j] = a[i][j] + b[i][j]
 				end
+=======
+			for i in 0...m -1  do 
+				c[i]= Array.new
+				for j in 0...n -1 do
+					c[i][j]=0
+				end			
+			for i in 0...m -1 do
+				for j in 0...n do
+					c[i][j] = a[i][j] + b[i][j]
+				end
+			end
+>>>>>>> funcion multiplicar
 			end
 			puts "\n La Matriz a es = "
 			a.each do |fila| puts fila.join(" ") end
@@ -37,11 +53,34 @@ m,n = a.size, a[1].size
 
         elsif opcion == 2  
 			   
+<<<<<<< HEAD
 			print "Introduce el primer numero: "
 			x = gets.chomp.to_i 
 			print "Introduce el segundo numero: "
 			y = gets.chomp.to_i
 			puts "La multiplicacion de #{x} y #{y} vale #{x*y}"
+=======
+			for i in 0...m do 
+				c[i]= Array.new
+				for j in 0...n do
+					c[i][j]=0
+				end
+			end	
+			
+			for i in 0...m do
+				for j in 0...n do
+					for k in 0...m do
+					c[i][j] = c[i][j] + (a[i][k] * b[k][j])
+					end
+				end
+			end
+			puts "\n La Matriz a es = "
+			a.each do |fila| puts fila.join(" ") end
+			puts "\n La Matriz b es = " 
+			b.each do |fila| puts fila.join(" ") end
+			puts "\n La multiplicaciión de la Matriz a y b es = "
+			c.each do |fila|puts fila.join(" ") end
+>>>>>>> funcion multiplicar
 				
 		    else
 			print "FIN\n"	
