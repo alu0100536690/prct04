09b9c0ff584f9f776d39fd31d81ab5c07f850d84
alu0100536690 <alu0100536690@ull.edu.es>
2013-10-07 21:42:04 +0100

    Tabulando el código

--- matrices.rb
+++ matrices.rb
@@ -14,53 +14,38 @@ m,n = a.size, a[1].size
 		opcion = gets.chomp.to_i
 		system("clear")# Borramos la pantalla
 
-
-
-
 		if opcion == 1 
 			 
-		
-		
-		for i in 0...m do 
-		c[i]= Array.new
-		
-			for j in 0...n do
-				c[i][j]=0
+			for i in 0...m do 
+				c[i]= Array.new
+				for j in 0...n do
+					c[i][j]=0
+				end
+			end	
+			
+			for i in 0...m do
+				for j in 0...n do
+					c[i][j] = a[i][j] + b[i][j]
+				end
 			end
-        end	
-        	
-        for i in 0...m do
-         for j in 0...n do
-           c[i][j] = a[i][j] + b[i][j]
-         end
-        end
 			puts "\n La Matriz a es = "
-			a.each do |fila|
-			puts fila.join(" ")
-        end
-        puts "\n La Matriz b es = "
-        b.each do |fila|
-        puts fila.join(" ")
-        end
-        puts "\n La Suma de la Matriz a y b es = "
-        c.each do |fila|puts fila.join(" ")
-             end
+			a.each do |fila| puts fila.join(" ") end
+			puts "\n La Matriz b es = " 
+			b.each do |fila| puts fila.join(" ") end
+			puts "\n La Suma de la Matriz a y b es = "
+			c.each do |fila|puts fila.join(" ") end
 
-
-        
-        
-
-                elsif opcion == 2  
+        elsif opcion == 2  
 			   
-				print "Introduce el primer numero: "
-				x = gets.chomp.to_i
-				print "Introduce el segundo numero: "
-				y = gets.chomp.to_i
-				puts "La multiplicacion de #{x} y #{y} vale #{x*y}"
-			      
-	
-		else
+			print "Introduce el primer numero: "
+			x = gets.chomp.to_i 
+			print "Introduce el segundo numero: "
+			y = gets.chomp.to_i
+			puts "La multiplicacion de #{x} y #{y} vale #{x*y}"
+				
+		    else
 			print "FIN\n"	
 		end
-	   system("sleep 3") # Nos esperamos 3 segundos para continuar
+	   system("sleep 15") # Nos esperamos 15 segundos para continuar
+	   system("clear")# Borramos la pantalla
 	end
