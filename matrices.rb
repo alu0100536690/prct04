6a524c263195f11e1ba22b40ae6eff693807518c
alu0100536690 <alu0100536690@ull.edu.es>
2013-10-07 20:44:48 +0100

    probando matriz

--- matrices.rb
+++ matrices.rb
@@ -1,9 +1,25 @@
+#! /home/ubuntu/.rvm/rubies/ruby-2.0.0-p247/bin/ruby
+print "Espesifique el tamaño de la matriz: "
+STDOUT.flush
+t=gets.chomp
 
-	#a = [[1,2,3],[4,5,6],[7,8,9]]
-	#b = [[2,3,4],[5,6,7],[8,9,10]]
+a=Array.new(t.to_i){Array.new(t.to_i)}
+b=Array.new(t.to_i){Array.new(t.to_i)}
+system("clear")	
+
+for index in 0 ... a.size*2
+	a[0][0] = gets.chomp.to_i
+end
+
+
+
+for index in 0 ... a.size
+	puts  "#{a[index].inspect}"
+end
+
 	opcion=-1
 	while opcion!=0 do
-		system("clear") # Borramos la pantalla
+		
 		puts "ELIGE OPCION"
 		puts "1 - Sumar matrices"
 		puts "2 - Multiplicar matrices"
@@ -11,6 +27,9 @@
 		print "Elige una opcion: "
 		opcion = gets.chomp.to_i
 		system("clear")# Borramos la pantalla
+
+
+
 
 		if opcion == 1 
 			 
